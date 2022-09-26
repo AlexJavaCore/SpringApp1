33816c9149f46231117d8349cd5d8f123491a3cd
DESKTOP-1SAP1JH\Александр <a.smolin@freeplay.by>
2022-09-26 08:56:33 +0300

    lesson 10

--- spring-app1/src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ spring-app1/src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,43 +1,24 @@
 package ru.alishev.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    private String name;
-    private int volume;
-
-    public String getName() {
-        return name;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
 
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-
-    // IoC
-    public MusicPlayer(Music music) { this.music = music; }
-
-    public MusicPlayer(){}
-
-    public void setMusicList(Music musicList){
-
-        this.music = music;
-    }
-
-    public void playMusic() {
-
-            System.out.println("Playing: " + music.getSong());
     }
 }
